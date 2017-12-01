3b3c970c0a9e65f39170c513b5dc0e589fc4ce10
rainbowBin <474174309@qq.com>
2017-12-01 17:35:56 +0800

    init

--- app/src/main/java/com/example/liubin/design/defalt/DefaultActivity.java
+++ app/src/main/java/com/example/liubin/design/defalt/DefaultActivity.java
@@ -30,7 +30,8 @@ public class DefaultActivity extends AppCompatActivity
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Action", null).show();//todo
+
             }
         });
 
--- app/src/main/java/com/example/liubin/design/material/MaterialActivity.java
+++ app/src/main/java/com/example/liubin/design/material/MaterialActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,6 +22,6 @@ public class MaterialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material);
+        Log.i("tag","add lo");
     }
-
 }
